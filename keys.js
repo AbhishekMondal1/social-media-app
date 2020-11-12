c43b58a08f85c16a418fac2ae9dcc43c2cae15b4
AbhishekMondal1 <abhishekmondal.official@gmail.com>
2020-11-12 21:26:13 +0530

    Signin added, token auth added

--- keys.js
+++ keys.js
@@ -1,3 +1,4 @@
 module.exports=({
-    MONGOURI:"mongodb+srv://abhi:P4ssNewSavedn0w@cluster0.ldotm.mongodb.net/test?retryWrites=true&w=majority"
+    MONGOURI:"mongodb+srv://abhi:P4ssNewSavedn0w@cluster0.ldotm.mongodb.net/test?retryWrites=true&w=majority",
+    JWT_SECRET:"secrettokenjwttoken" 
 })
--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,11 @@ const router = express.Router()
 const mongoose = require('mongoose')
 const User = mongoose.model('User')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const {JWT_SECRET} = require('../keys')
+
+
+
 
 router.post('/signup',(req,res)=>{
    const {name,email,password} = req.body
@@ -36,5 +41,34 @@ router.post('/signup',(req,res)=>{
         console.log(err)
     })   
 })
+
+
+router.post('/signin',(req,res)=>{
+    const {email,password} = req.body
+    if(!email || !password){
+        return res.status(422).json({error:"Please enter Email or Password"})
+    }
+    User.findOne({email:email})
+    .then(savedUser=>{
+        if(!savedUser){
+           return res.status(422).json({error:"Invalid Email or Password"})
+        }
+        bcrypt.compare(password,savedUser.password)
+        .then(doMatch=>{
+            if(doMatch){
+               // res.json({message:"successfully signed in"})
+                const token = jwt.sign({_id:savedUser._id},JWT_SECRET)
+                res.json({token})
+            }
+            else{
+                return res.status(422).json({error:"Invalid Email or Password"}) 
+            }
+        })
+        .catch(err=>{
+            console.log(err)
+        })
+    })
+})
+
 
 module.exports = router
