f0f4b8d9454413c6d53080fce929367ab87b5313
AbhishekMondal1 <abhishekmondal.official@gmail.com>
2020-11-22 16:16:26 +0530

    Added Fetch Post in Home

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -1,46 +1,41 @@
-import React from "react";
+import React,{useState, useEffect} from "react";
 
 const Home = () => {
+  const [data, setData] = useState([])
+  useEffect(()=>{
+    fetch('./allpost', {
+      headers: {
+        "Authorization":"Bearer "+localStorage.getItem("jwt")
+      }
+    }).then(res => res.json())
+      .then(result => {
+        console.log(result)
+      setData(result.posts)
+    })
+  },[])
   return (
     <div className="home">
-      <div className="card home-card">
-        <h5>Jasprit</h5>
-        <div className="card-image">
-          <img src="https://cdn.pixabay.com/photo/2020/10/16/12/14/festival-5659357_960_720.jpg" />
-        </div>
-        <div className="card-content">
-          <i class="material-icons">favorite</i>
-          <h6>title</h6>
-          <p>This is a post</p>
-          <input type="text" placeholder="add a comment" />
-        </div>
-      </div>
-      <div className="card home-card">
-        <h5>Jasprit</h5>
-        <div className="card-image">
-          <img src="https://cdn.pixabay.com/photo/2020/10/16/12/14/festival-5659357_960_720.jpg" />
-        </div>
-        <div className="card-content">
-          <i class="material-icons">favorite</i>
-          <h6>title</h6>
-          <p>This is a post</p>
-          <input type="text" placeholder="add a comment" />
-        </div>
-      </div>
-      <div className="card home-card">
-        <h5>Jasprit</h5>
-        <div className="card-image">
-          <img src="https://cdn.pixabay.com/photo/2020/10/16/12/14/festival-5659357_960_720.jpg" />
-        </div>
-        <div className="card-content">
-          <i class="material-icons">favorite</i>
-          <h6>title</h6>
-          <p>This is a post</p>
-          <input type="text" placeholder="add a comment" />
-        </div>
-      </div>
+      {
+        data.map(item => {
+          return (
+          <div className="card home-card" key={item._id}>
+            <h5>{item.postedBy.name}</h5>
+            <div className="card-image">
+              <img src={item.photo}/>
+            </div>
+            <div className="card-content">
+              <i class="material-icons">favorite</i>
+        <h6>{item.title}</h6>
+              <p>{item.body}}</p>
+              <input type="text" placeholder="add a comment" />
+            </div>
+            </div>
+          )
+        })
+      }
+      
     </div>
-  );
-};
+  )
+}
 
 export default Home;
