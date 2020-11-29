91b372e9e22efe86d62f78d0c770f13926c548ce
AbhishekMondal1 <abhishekmondal.official@gmail.com>
2020-11-29 19:12:50 +0530

    Added Like Unlike

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -1,7 +1,8 @@
-import React,{useState, useEffect} from "react";
-
+import React,{useState, useEffect, useContext} from "react";
+import {UserContext} from '../../App'
 const Home = () => {
   const [data, setData] = useState([])
+  const {state,dispatch} = useContext(UserContext)
   useEffect(()=>{
     fetch('./allpost', {
       headers: {
@@ -12,25 +13,96 @@ const Home = () => {
         console.log(result)
       setData(result.posts)
     })
-  },[])
+  }, [])
+  
+  const likepost = (id) => {
+    fetch('/like', {
+      method: "put",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + localStorage.getItem("jwt")
+      },
+      body: JSON.stringify({
+        postId: id
+      })
+    }).then(res => res.json())
+      .then(result => {
+        //console.log(result)
+        const newData = data.map(item => {
+          if (item._id == result._id) {
+            return result
+          }
+          else {
+            return item
+          }
+        })
+        setData(newData)
+      }).catch(err => {
+        console.log(err)
+      })
+}
+  const unlikepost = (id) => {
+    fetch("/unlike", {
+      method: "put",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + localStorage.getItem("jwt"),
+      },
+      body: JSON.stringify({
+        postId: id,
+      }),
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        //console.log(result)
+        const newData = data.map((item) => {
+          if (item._id == result._id) {
+            return result;
+          } else {
+            return item;
+          }
+        });
+        setData(newData);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+}
+
   return (
     <div className="home">
       {
         data.map(item => {
           return (
-          <div className="card home-card" key={item._id}>
-            <h5>{item.postedBy.name}</h5>
-            <div className="card-image">
-              <img src={item.photo}/>
+            <div className="card home-card" key={item._id}>
+              <h5>{item.postedBy.name}</h5>
+              <div className="card-image">
+                <img src={item.photo} />
+              </div>
+              <div className="card-content">
+                <i className="material-icons">favorite</i>
+                {item.likes.includes(state._id) ?
+                  <i
+                    className="material-icons"
+                    onClick={() => unlikepost(item._id)}
+                  >
+                    thumb_down
+                </i>
+                  :
+                  <i
+                    className="material-icons"
+                    onClick={() => likepost(item._id)}
+                  >
+                    thumb_up
+                </i>
+                }
+                <h6>{item.likes.length} likes</h6>
+                <h6>{item.title}</h6>
+                <p>{item.body}}</p>
+                <input type="text" placeholder="add a comment" />
+              </div>
             </div>
-            <div className="card-content">
-              <i class="material-icons">favorite</i>
-        <h6>{item.title}</h6>
-              <p>{item.body}}</p>
-              <input type="text" placeholder="add a comment" />
-            </div>
-            </div>
-          )
+          );
         })
       }
       
